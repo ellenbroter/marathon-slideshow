635ed323e6cb7c5cd02dcedf97d4d875dc9df5dd
Ellen Brøter <143997330+ellenbroter@users.noreply.github.com>
2023-11-01 13:51:00 +0100

    Slideshow setup

--- js/app.js
+++ js/app.js
@@ -1,84 +1,69 @@
-/**
- * 	SLIDESHOW
- * */
+const slideshow = document.querySelector('.slideshow');
+const slides = slideshow.querySelectorAll('.slideshow__slide');
+const controls = slideshow.querySelectorAll('.slideshow__control-button');
+const dots = slideshow.querySelectorAll('.slideshow__dot');
+const counter = slideshow.querySelector('.slideshow__counter');
 
-// 1. get a list of all DOM elements with class "slideshow", in case we have several slideshows in the same page
-const slideshows = document.querySelectorAll('.slideshow');
+let index = 0;
 
-// 2. and for each slideshow of all slideshows...
-slideshows.forEach(slideshow => {
-	// 3. get all necessary DOM elements inside each slideshow element
-	const slides = slideshow.querySelectorAll('.slideshow__slide');
-	const previousButton = slideshow.querySelector('.slideshow__previous-button');
-	const nextButton = slideshow.querySelector('.slideshow__next-button');
-	const dotButtons = slideshow.querySelectorAll('.slideshow__dot-button');
+const totalSlides = slides.length;
+const lastIndex = slides.length - 1;
 
-	// 4. define a variable for keeping track of the current position
-	let currentSlideIndex = 0;
+const setIndex = (newIndex) => {
+	index = newIndex;
+};
 
-	// 4.1. and a variable to store the last index position
-	const lastSlideIndex = slides.length - 1;
+const decreaseIndex = () => {
+	if (index > 0) {
+		index = index - 1;
+	} else {
+		index = lastIndex;
+	}
+};
 
-	// 5. define a function that increments the index position
-	const goToNextSlide = () => {
-		if (currentSlideIndex === lastSlideIndex) {
-			currentSlideIndex = 0;
-		} else {
-			currentSlideIndex += 1;
-		}
-	};
+const increaseIndex = () => {
+	if (index < lastIndex) {
+		index = index + 1;
+	} else {
+		index = 0;
+	}
+};
 
-	// 5. define a function that decrements the index position
-	const goToPreviousSlide = () => {
-		if (currentSlideIndex === 0) {
-			currentSlideIndex = lastSlideIndex;
-		} else {
-			currentSlideIndex -= 1;
-		}
-	};
-
-	// 6. define a function that sets the index position to a specific value
-	const goToSingleSlide = new_slide_index => {
-		currentSlideIndex = new_slide_index;
-	};
-
-	// 7. define a function that updates the slides/buttons based on the current index position
-	const renderSlideshow = () => {
-		slides.forEach((slide, slideIndex) => {
-			if (slideIndex === currentSlideIndex) {
-				slide.classList.add('slideshow__slide--visible');
-			} else {
-				slide.classList.remove('slideshow__slide--visible');
-			}
-		});
-
-		dotButtons.forEach((dot_button, dot_index) => {
-			if (dot_index === currentSlideIndex) {
-				dot_button.classList.add('slideshow__dot-button--active');
-			} else {
-				dot_button.classList.remove('slideshow__dot-button--active');
-			}
-		});
-	};
-
-	// 8. run "goToPreviousSlide" and then "renderSlideshow" when I "click" on "previousButton"
-	previousButton.addEventListener('click', () => {
-		goToPreviousSlide();
-		renderSlideshow();
+const renderSlideClass = () => {
+	slides.forEach(slide => {
+		slide.classList.remove('slideshow__slide--visible');
 	});
 
-	// 9. run "goToNextSlide" and then "renderSlideshow" when I "click" on "nextButton"
-	nextButton.addEventListener('click', () => {
-		goToNextSlide();
-		renderSlideshow();
-	});
+	slides[index].classList.add('slideshow__slide--visible');
+}
 
-	// 10. for each dot button...
-	dotButtons.forEach((dotButton, dotIndex) => {
-		// 11. run "goToSingleSlide" using the dotIndex and then "renderSlideshow" when I "click" on "dotButton"
-		dotButton.addEventListener('click', () => {
-			goToSingleSlide(dotIndex);
-			renderSlideshow();
-		});
-	});
+const renderCounter = () => {
+	counter.textContent = `${index + 1} of ${totalSlides}`;
+}
+
+const changeSlide = (event) => {
+	const button = event.currentTarget;
+
+	if (button.dataset.direction === 'previous') {
+		decreaseIndex();
+	}
+
+	if (button.dataset.direction === 'next') {
+		increaseIndex();
+	}
+
+	if (button.dataset.index) {
+		setIndex(parseInt(button.dataset.index));
+	}
+
+	renderSlideClass();
+	renderCounter();
+}
+
+controls.forEach(button => {
+	button.addEventListener('click', changeSlide);
+});
+
+dots.forEach(button => {
+	button.addEventListener('click', changeSlide);
 });
